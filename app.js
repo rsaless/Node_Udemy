15474482bba2512a4b862f451214f13e0cf66fe9
Rafael <rspavarina@gmail.com>
2021-05-01 21:48:54 -0300

    using ejs

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs =  require('express-handlebars');
+//const expressHbs =  require('express-handlebars');
 
 const app = express();
 
-app.set('view engine','pug');
+//app.set('view engine','pug');
+//app.engine('hbs', expressHbs({layoutsDir: 'views/layouts',  defaultLayout:  'main-layout', extname: 'hbs'}));
+//app.set('view engine','hbs');
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts',  defaultLayout:  'main-layout', extname: 'hbs'}));
-app.set('view engine','hbs');
+app.set('view engine','ejs');
 app.set('views','views');
 
 const adminData = require('./routes/admin');
@@ -23,7 +24,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     //res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-    res.status(404).render('404', {docTitle:'Page not found'});
+    res.status(404).render('404', {docTitle:'Page not found', path:'aaaa'});
 });
 
 app.listen(3000);
