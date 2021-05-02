34b3573842dc627e29abd1f3ceaf2932232c5f03
Rafael <rspavarina@gmail.com>
2021-05-01 22:03:48 -0300

    cleanup

--- app.js
+++ app.js
@@ -2,13 +2,8 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-//const expressHbs =  require('express-handlebars');
 
 const app = express();
-
-//app.set('view engine','pug');
-//app.engine('hbs', expressHbs({layoutsDir: 'views/layouts',  defaultLayout:  'main-layout', extname: 'hbs'}));
-//app.set('view engine','hbs');
 
 app.set('view engine','ejs');
 app.set('views','views');
@@ -23,7 +18,6 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    //res.status(404).sendFile(path.join(__dirname,'views','404.html'));
     res.status(404).render('404', {docTitle:'Page not found', path:'aaaa'});
 });
 
